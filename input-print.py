3152cec3775f7ba2caa911494794179ecbd4f8b4
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-01 06:35:26 +0200

    some other experiments with input()

--- input-print.py
+++ input-print.py
@@ -1,2 +1,6 @@
 name = input("Enter your name: ")
 print("Hello", name)
+
+my_string = input("Please enter somenthing:")
+print("You have entered:", my_string)
+print("Your string is " + str(len(my_string)) + " characters long.")
