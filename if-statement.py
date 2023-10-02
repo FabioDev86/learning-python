d76ee411614e44c55efb4a8e77fc40914a582eb3
Fabio Bauleo <fabio.bauleo.developer@gmail.com>
2023-10-02 06:20:12 +0200

    if statement

---
+++ if-statement.py
@@ -0,0 +1,5 @@
+age = int(input("Qual è la tua età? " ))
+if age < 18:
+    print("Sei minorenne")
+else:
+    print("Sei maggiorenne")
--- input-print.py
+++ input-print.py
@@ -1,6 +1,11 @@
-name = input("Enter your name: ")
-print("Hello", name)
+# name = input("Enter your name: ")
+# print("Hello", name)
 
-my_string = input("Please enter somenthing:")
-print("You have entered:", my_string)
-print("Your string is " + str(len(my_string)) + " characters long.")
+# my_string = input("Please enter somenthing:")
+# print("You have entered:", my_string)
+# print("Your string is " + str(len(my_string)) + " characters long.")
+
+n1 = input('First number is: ')
+n2 = input('Second number is: ')
+user_sum = str(float(n1) + float(n2))
+print("The sum of " + str(n1) + " and " + str(n2) + " is " + user_sum)
